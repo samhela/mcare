a5bc050cee0cd276bc3e9ab24175bb5da3f9087c
netsanet11 <nmtame11@gmail.com>
2017-03-19 19:59:36 -0700

    Part Sunny

--- src/main/java/edu/mum/cs/waa/domain/HealthFacility.java
+++ src/main/java/edu/mum/cs/waa/domain/HealthFacility.java
@@ -1,14 +1,60 @@
 package edu.mum.cs.waa.domain;
 
+import java.io.Serializable;
 import java.util.List;
 
-public class HealthFacility {
-	
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class HealthFacility implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3418261888039156767L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
 	private String name;
 	private String location;
+	
 	private List<Address>addresses;
 	private List<Physician>physicians;
+	//private List<>
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public List<Address> getAddresses() {
+		return addresses;
+	}
+	public void setAddresses(List<Address> addresses) {
+		this.addresses = addresses;
+	}
+	public List<Physician> getPhysicians() {
+		return physicians;
+	}
+	public void setPhysicians(List<Physician> physicians) {
+		this.physicians = physicians;
+	}
+	
 	
 	
 		
