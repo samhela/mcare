bf887089d0952bc11c022c092f6d830ba9e5d0d3
netsanet11 <nmtame11@gmail.com>
2017-03-20 12:13:50 -0700

    Repositary Changes

--- src/main/java/edu/mum/cs/waa/repository/InsurancePackageRepository.java
+++ src/main/java/edu/mum/cs/waa/repository/InsurancePackageRepository.java
@@ -1,5 +1,8 @@
 package edu.mum.cs.waa.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,8 @@ import edu.mum.cs.waa.domain.InsurancePackage;
 public interface InsurancePackageRepository extends CrudRepository<InsurancePackage, Long> {
 
 	//Add some custom implementations
+	@Query("Select p from InsurancePackage p where p.id = :userId ")
+	List<InsurancePackage> findInsurancePackageByUserId(long userId);
+
+	
 }
