897042932da8fadfc383850b133f76fa3b101010
netsanet11 <nmtame11@gmail.com>
2017-03-19 19:25:02 -0700

    Address JPA set

--- src/main/java/edu/mum/cs/waa/domain/Address.java
+++ src/main/java/edu/mum/cs/waa/domain/Address.java
@@ -1,11 +1,48 @@
 package edu.mum.cs.waa.domain;
 
-public class Address {
-    private long id;
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+@Entity
+public class Address implements Serializable {
+	
+	private long id;
 	private String zipCode;
 	private String street;
 	private String city;
 	private String state;
+	
+    public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	
 	
 	
 	
